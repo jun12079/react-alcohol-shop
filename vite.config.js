231731a46a84530c22ce11dbcd048d73f6a79d68
jun12079 <jeffery851007@gmail.com>
2025-03-24 08:50:19 +0800

    Optimize code

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-alcohol-shop/' : '/',
+  base: import.meta.env === 'production' ? '/react-alcohol-shop/' : '/',
   plugins: [react()],
 })
