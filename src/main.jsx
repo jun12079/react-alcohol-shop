001d3def140005574066e18e19a0d7a3e6472fff
jun12079 <jeffery851007@gmail.com>
2025-03-31 10:53:36 +0800

    Update bootstrap5.3 SCSS & fix navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.jsx'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap';
 import './assets/styles/all.scss'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
